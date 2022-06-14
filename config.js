8044e12bd99b1152c80d1a340ec1544efa27d617
violetflare@protonmail.com <violetflare@protonmail.com>
2022-06-14 22:42:19 +0200

    fix

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    TOKEN_PROD: "OTg2MzQ2MDkxODExMzA3NTgy.GaIJEV.wyAerPlQEqEhezy5yV-PVRkcFE65N1qVq2s_i0",
-    TOKEN_DEV: "OTg2MzQ3MDM3NjQzNjY5NjA0.Go5UUy.w1M-Q3pGMx7bloP5_qLE7K174uJaytuQnY4a0c"
-}
